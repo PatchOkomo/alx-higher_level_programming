4cc5adc422efdd89ea1c617dad77d9a8890cd903
root <osodopatch@yahoo.com>
2021-11-16 19:39:16 +0000

    A different approach for 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alph in range(97, 123):
-    if chr(alph) is not 'q' and chr(alph) is not 'e':
-        print("{}".format(chr(alph)), end="")
+for alph in range(ord('a'), ord('z')+1):
+    if alph is not (ord('q')) and alph is not (ord('e')):
+        print('{}'.format(chr(alph)), end='')
