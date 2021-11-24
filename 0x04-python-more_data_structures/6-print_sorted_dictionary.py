47ba2854fc4724c9af4c8160703cba1a79927229
root <osodopatch@yahoo.com>
2021-11-24 20:31:07 +0000

    Initial commit for 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    [print("{}: {}".format(i, a_dictionary[k])) for i in sorted(a_dictionary)]
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, value))
