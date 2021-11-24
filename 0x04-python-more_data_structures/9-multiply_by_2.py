88bdc960ca05c6c4da32647c77e4cbb91af0cf46
root <osodopatch@yahoo.com>
2021-11-24 20:41:04 +0000

    A longer approach for 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return ({i: a_dictionary[i] * 2 for i in a_dictionary})
+    if a_dictionary is not None:
+        b_dictionary = {}
+        temp = {}
+        for key, value in a_dictionary.items():
+            new_val = value * 2
+            temp = {key: new_val}
+            b_dictionary.update(temp)
+        return (b_dictionary)
+    return None
