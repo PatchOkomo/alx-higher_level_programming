fbd098a40b3ce55a2dcfc0eab5628c1b1cd22694
root <osodopatch@yahoo.com>
2021-11-24 20:46:54 +0000

    Pycode guidelines for 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,17 +3,17 @@ def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
-    rom_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    romdict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     num = 0
 
     for i in range(len(roman_string)):
-        if rom_dict.get(roman_string[i], 0) == 0:
+        if romdict.get(roman_string[i], 0) == 0:
             return (0)
 
         if (i != (len(roman_string) - 1) and
-                rom_dict[roman_string[i]] < rom_dict[roman_string[i + 1]]):
-                num += rom_dict[roman_string[i]] * -1
+                romdict[roman_string[i]] < romdict[roman_string[i + 1]]):
+            num += romdict[roman_string[i]] * -1
 
         else:
-            num += rom_dict[roman_string[i]]
+            num += romdict[roman_string[i]]
     return (num)
