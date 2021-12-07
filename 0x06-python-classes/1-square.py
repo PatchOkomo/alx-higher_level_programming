bf67b572586e6b7eeb4645b68fd9089b9c20f9bc
root <osodopatch@yahoo.com>
2021-12-07 19:17:43 +0000

    Initial commit for 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A square with an attribute of size"""
+class Square:
+    """represents square with size attribute"""
+    def __init__(self, size):
+        """
+        Initialize a square with attribute size
+        
+        Args:
+        size (int): The length of the side of the square
+        """
+        self.__size = size
