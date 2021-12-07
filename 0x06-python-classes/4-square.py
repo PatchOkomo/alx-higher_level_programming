a2c7d50e7d43582a6778a52308c67c82222b5fa6
root <osodopatch@yahoo.com>
2021-12-07 20:03:30 +0000

    Initial commit for 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""A square with an attribute of size
+defines a square with private attribute size and validates size
+can access and update size
+can get area
+"""
+
+
+class Square:
+    """
+    Class Square definition
+
+    Args:
+    size (int): The length of the side of the square
+
+    functions:
+        __init__(self, size)
+        size(self)
+        size(self, value)
+        area(self)
+    """
+    def __init__(self, size=0):
+        """
+        Initialize a square with attribute size
+
+        Attribute(s):
+        __size (int): The length of the side of the square. Default = 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """"
+        Getter
+        Return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+        Args:
+            value: sets size to value, if int and >= 0
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Returns the area of the square
+
+        Returns:
+        area
+        """
+        return (self.__size)**2
