62469d719306faecd4413083052cf44e4c7826fe
root <osodopatch@yahoo.com>
2021-12-21 22:12:07 +0000

    Initial commit for *.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,8 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
     try:
         content = load_from_json_file("add_item.json")
@@ -16,4 +17,3 @@ if __name__ == "__main__":
         content = []
     content.extend(sys.argv[1:])
     save_to_json_file(content, "add_item.json")
-
