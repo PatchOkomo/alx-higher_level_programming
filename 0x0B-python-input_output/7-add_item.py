04e4c1ed31701ee6aa38a931b51de87337a1e9f7
root <osodopatch@yahoo.com>
2021-12-21 22:04:05 +0000

    Initial commit for *.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,15 +4,16 @@ Module 9-add_item
 Contains function that adds and saves to Python obj to JSON file; loads objects
 #Add all arguments to a Python list and save them to a file.
 """
+import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    try:
+        content = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        content = []
+    content.extend(sys.argv[1:])
+    save_to_json_file(content, "add_item.json")
 
-filename = "add_item.json"
-
-try:
-    existing_content = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_content = []
